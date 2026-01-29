848df50cc338cd2969b6909eb47b941dd2e6b336
Ikmalia <ikmaliashazlin123@gmail.com>
2026-01-29 11:03:43 +0800

    m to km

--- code.cpp
+++ code.cpp
@@ -9,7 +9,7 @@ int main() {
     double totalPrice;
 
     // Input
-    cout << "Enter distance (km): ";
+    cout << "Enter distance kmmm: ";
     cin >> distanceKm;
 
     cout << "Enter food price (RM): ";
